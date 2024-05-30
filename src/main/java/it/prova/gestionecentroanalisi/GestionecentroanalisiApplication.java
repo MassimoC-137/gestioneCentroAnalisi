e38d3e1b36555fe4e2dfa54644e680c5aab16cea
MassimoC-137 <massimo.caramanna@gmail.com>
2024-05-30 11:29:36 +0200

    commit iniziale

---
+++ src/main/java/it/prova/gestionecentroanalisi/GestionecentroanalisiApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionecentroanalisi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionecentroanalisiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionecentroanalisiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/prova/gestionecentroanalisi/GestionecentroanalisiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.gestionecentroanalisi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionecentroanalisiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
